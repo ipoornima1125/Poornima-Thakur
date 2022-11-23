7bf94b8ea884781193c658bc2a97f6dc5e26f787
ipoornima1125 <118872471+ipoornima1125@users.noreply.github.com>
2022-11-23 09:57:12 +0530

    Add files via upload

---
+++ python1.p.py
@@ -0,0 +1,38 @@
+print("hello world")
+message= " meet me tonight"
+print(message)
+number_1= int(input('enter your first number:'))
+number_2= int(input('enter your second number:'))
+sum= print(number_1+number_2)
+
+a=10
+b=20
+print(a+b)
+
+
+a= int(input(' enter number:'))
+b=int(input(' enter number:'))
+c=int(input(' enter number:'))
+       
+
+
+       
+
+
+
+       
+
+if a>b and a>c:
+    print("a")
+if b>a and b>c:
+    print("b")
+else:
+    print("c")
+
+
+
+
+
+
+
+    
